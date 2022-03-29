1af73e12361738853aba9900a776c355338164fe
Ignaz Protagonist <pilibili@protonmail.com>
2022-03-29 02:41:47 -0500

    getting there with the scaling..slowly

--- public/JS/roulette1.js
+++ public/JS/roulette1.js
@@ -1,20 +1,39 @@
-//load images and canvas to script
-                    //const XXXchip = document.getElementById("imageLoader").children[0];
-                    //const XXboard = document.getElementById("imageLoader").children[1];
-let board =new Image();
-let chip =new Image();
-chip.src = "chip.png"
+
+const canvas = document.getElementById("viewScreen");
+const ctx = canvas.getContext("2d");
+
+//dimensions of display in pixels
+let w = window.innerWidth;
+let h = window.innerHeight;
+
+//dimensions of canvas in terms of screen
+canvas.style.width=`${w/2}px`;
+canvas.style.height=`${h/2}px`;
+
+let unit = Math.min(w/2,h/2);
+console.log(unit);
+console.log(typeof unit);
+
+let dice = [new Image(),new Image(),new Image(),new Image(),new Image(),new Image()];
+let midice = [new Image(),new Image(),new Image(),new Image(),new Image(),new Image()];
+
+dice[0].onload = function () {
+    ctx.clearRect(0,0,w/2,h/2);
+    ctx.drawImage(dice[0],0,0,unit,unit);//draw board
+}
 
 
-var canvas = document.getElementById("viewScreen");
-var ctx = canvas.getContext("2d");
+for (let i=0; i<6; i++){
+    dice[i].src= `${i+1}.png`;
+    midice[i].src= `m${i+1}.png`;
+/*
+    dice[i].width=`${unit}px`;
+    dice[i].height=`${unit}px`;
+    midice[i].width=`${unit}px`;
+    midice[i].height=`${unit}px`;
+    */
+}
 
-
-board.onload = function () {
-    ctx.clearRect(0,0,1500,705);
-    ctx.drawImage(board,0,0);//draw board
- }
-board.src = "board.png";
 
 
 
@@ -22,14 +41,9 @@ board.src = "board.png";
 
 const bet = document.getElementById("betButtons");
 bet.addEventListener('click',placeBet);
-const bets = [0,0,0,0,0,0];
 
 
-ctx.clearRect(0,0,1500,705);
-ctx.drawImage(board,0,0);//draw board
 
-
-pos = [[200,650],[200,850],[200,1050],[400,650],[400,850],[400,1050]]
 
 let x = 0;
 //when a bet is placed
@@ -38,13 +52,20 @@ function placeBet(event){
     if(!event.target.value) return;//if they click between the buttons
     else{
 
-        let b = parseInt(event.target.value);
+        let b = event.target.value;
         
-        bets[b]++;
+        
+        console.log(b);
+////need to replace this with a proper timer function
+        for (let k=0; k<100; k++){
+            ctx.drawImage(midice[Math.floor(Math.random() * 6)],0,0,450,447, 0,0,unit,unit);
+            x=Math.floor(Math.random() * 6)+1
+            ctx.drawImage(dice[x-1],0,0,450,447, 0,0,unit,unit);
+        }
 
-        //x and y are inverted in the array, fix that for big version
-        //+bets*5 is to offset new chips
-        ctx.drawImage(chip, pos[b][1]+bets[b]*5, pos[b][0]-bets[b]*5);
+        console.log(x);
+
+
         
 
         
@@ -53,33 +74,7 @@ function placeBet(event){
 }
 
 
-//when button is clicked
-function draw(){
-    x++;
 
-    
-    
-
-    //draw image elements on canvas 
-
-    //clear previous drawing
-    ctx.clearRect(0,0,1500,705);
-
-    //draw background
-    ctx.drawImage(board,0,0);
-
-
-    
-
-    ctx.drawImage(chip, pos[x%6][1], pos[x%6][0]);
-   
-    
-
-}
-
-
-
-//document.querySelector('#create-post').addEventListener('submit', newFormHandler);
 
 async function back (event){
     event.preventDefault();
