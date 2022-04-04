ac87b097b98ccdf7b1d00e1aacbfe33f4b967798
jasonjayoo <94942855+jasonjayoo@users.noreply.github.com>
2022-04-03 17:09:04 -0700

    server.js

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static(path.join(__dirname, 'images')));
+app.use(express.static(path.join(__dirname, 'images')));
 
 
 // app.get("/static", (req, res) => {/////////////////what is this??
