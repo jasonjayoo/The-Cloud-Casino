03ba99579024ac360e13bb2725d8ea18f31e2ed9
Ignaz Protagonist <pilibili@protonmail.com>
2022-04-08 00:06:54 -0500

    after entering email, message displays and we return to lobby

--- public/JS/bonus.js
+++ public/JS/bonus.js
@@ -20,7 +20,7 @@ async function getBonus(collector){
 
                 setTimeout(()=>{
                     document.location.replace('/dashboard');
-                },1000)
+                },2000)
 
             })
         }else{
